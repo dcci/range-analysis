f9677dfd032f811ed545977690f8d7aead7cccaa
Davide Italiano <dccitaliano@gmail.com>
2017-05-28 18:56:08 -0700

    [RangeProp] Add an inteprocedural client.

--- src/RangePropagation/RangePropagation.cpp
+++ src/RangePropagation/RangePropagation.cpp
@@ -12,7 +12,7 @@ struct RangePropagation : public FunctionPass {
   RangePropagation() : FunctionPass(ID) {}
 
   bool runOnFunction(Function &F) override {
-    IntraProceduralRA<Cousot> &RA = getAnalysis<IntraProceduralRA<Cousot>>();
+    IntraProceduralRA<CropDFS> &RA = getAnalysis<IntraProceduralRA<CropDFS>>();
     errs() << "Analysis for function: " << F.getName() << "\n";
     for (BasicBlock &BB : F) {
       for (Instruction &I : BB) {
@@ -28,11 +28,43 @@ struct RangePropagation : public FunctionPass {
   }
 
   void getAnalysisUsage(AnalysisUsage &AU) const override {
-    AU.addRequired<IntraProceduralRA<Cousot>>();
+    AU.addRequired<IntraProceduralRA<CropDFS>>();
   }
 };
+
+// Interprocedural version.
+struct IPRangePropagation : public ModulePass {
+  static char ID;
+  IPRangePropagation() : ModulePass(ID) {}
+
+  bool runOnModule(Module &M) override {
+    InterProceduralRA<CropDFS> &RA = getAnalysis<InterProceduralRA<CropDFS>>();
+    for (Function &F : M) {
+      errs() << "Analysis for function: " << F.getName() << "\n";
+      for (BasicBlock &BB : F) {
+        for (Instruction &I : BB) {
+          const Value *V = &I;
+          Range R = RA.getRange(V);
+          if (!R.isUnknown()) {
+            R.print(errs());
+            errs() << I << "\n";
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  void getAnalysisUsage(AnalysisUsage &AU) const override {
+    AU.addRequired<InterProceduralRA<CropDFS>>();
+  }
+};
+
 }
 
 char RangePropagation::ID = 0;
 static RegisterPass<RangePropagation>
     X("rangeprop", "Intraprocedural Range Propagation", false, false);
+char IPRangePropagation::ID = 0;
+static RegisterPass<IPRangePropagation>
+    Y("iprangeprop", "Interprocedural Range Propagation", false, false);
